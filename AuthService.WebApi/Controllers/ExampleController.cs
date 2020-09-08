e1fa7df8cba52dbcc1b15641a7901de4da9a817c
Mikhail Doshevsky <m.doshevsky@gmail.com>
2020-09-08 23:57:19 +0300

    Add Example auth controller

---
+++ AuthService.WebApi/Controllers/ExampleController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/example")]
+    public class ExampleController : ControllerBase
+    {
+        [HttpGet("no-auth")]
+        public ActionResult GetNoAuth()
+        {
+            return Ok();
+        }
+
+        [HttpGet("auth")]
+        [Authorize]
+        public ActionResult GetAuth()
+        {
+            return Ok();
+        }
+
+        [HttpGet("auth-regular")]
+        [Authorize(Roles = "Regular")]
+        public ActionResult GetAuthUser()
+        {
+            return Ok();
+        }
+
+        [HttpGet("auth-admin")]
+        [Authorize(Roles = "Admin")]
+        public ActionResult GetAuthAdmin()
+        {
+            return Ok();
+        }
+    }
+}
